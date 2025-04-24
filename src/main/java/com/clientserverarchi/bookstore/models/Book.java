24cbd441f6ea822f87b4d60d0c7205383b585491
senukafer12 <senukafernando1212@gmail.com>
2025-04-24 10:45:05 +0530

    feat: created the basic structure

---
+++ src/main/java/com/clientserverarchi/bookstore/models/Book.java
@@ -0,0 +1,77 @@
+package com.clientserverarchi.bookstore.models;
+
+public class Book {
+    private Long id;
+    private String title;
+    private Long authorId;
+    private String isbn;
+    private int publicationYear;
+    private double price;
+    private int stockQuantity;
+
+    public Book(Long id, String title, Long authorId, String isbn, int publicationYear, double price, int stockQuantity) {
+        this.id = id;
+        this.title = title;
+        this.authorId = authorId;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
